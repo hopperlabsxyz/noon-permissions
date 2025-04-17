7f3abb0eae17daddfbec7da3e4f1082fd41d4f58
pedro-noon <pierre@dclflabs.xyz>
2025-04-17 20:13:30 +0200

    Update - add tests

--- test/roles/manager_noon_mainnet/index.t.sol
+++ test/roles/manager_noon_mainnet/index.t.sol
@@ -11,6 +11,22 @@ address constant ASSET = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT
 
 address constant sUSN = 0xE24a3DC889621612422A64E6388927901608B91D;
 address constant USN = 0xdA67B4284609d2d48e5d10cfAc411572727dc1eD;
+address constant EULER_CONNECTOR = 0x0C9a3dd6b8F28529d72d7f9cE918D493519EE383;
+
+address constant BUNNI_HUB = 0x000000DCeb71f3107909b1b748424349bfde5493;
+address constant WITHDRAW_HANDLER = 0x0DaBc0D9B270c9B0C4C77AaCeAa712b56D0F9178;
+
+interface IEVC {
+    struct BatchItem {
+        address targetContract;
+        address onBehalfOfAccount;
+        uint256 value;
+        bytes data;
+    }
+
+    function batch(BatchItem[] memory items) external payable;
+}
+
 
 contract ManagerNoonMainnet is BaseTest {
     function setUp() public {
@@ -104,5 +120,77 @@ contract ManagerNoonMainnet is BaseTest {
         vm.expectRevert();
         vm.prank(manager);
         role.execTransactionWithRole(sUSN, 0, data, 0, ROLE_KEY, false);
+    }
+
+    function test_euler_connector_batch() public {
+        // Create a simple batch operation
+        IEVC.BatchItem[] memory items = new IEVC.BatchItem[](1);
+        items[0] = IEVC.BatchItem({
+            targetContract: TARGET,
+            onBehalfOfAccount: avatar,
+            value: 0,
+            data: abi.encodeWithSelector(Vault(TARGET).settleDeposit.selector, 42)
+        });
+
+        bytes memory data = abi.encodeWithSelector(
+            IEVC(EULER_CONNECTOR).batch.selector,
+            items
+        );
+
+        vm.prank(manager);
+        role.execTransactionWithRole(EULER_CONNECTOR, 0, data, 0, ROLE_KEY, false);
+    }
+
+    function test_approve_bunni() public {
+        // Test approving USN for Bunni hub
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            BUNNI_HUB,
+            10
+        );
+        vm.prank(manager);
+        role.execTransactionWithRole(USN, 0, data, 0, ROLE_KEY, false);
+
+        // Test approving USDT for Bunni hub
+        data = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            BUNNI_HUB,
+            10
+        );
+        vm.prank(manager);
+        role.execTransactionWithRole(ASSET, 0, data, 0, ROLE_KEY, false);
+    }
+
+    function test_susn_withdraw() public {
+        // Test withdrawing from sUSN to withdrawal handler
+        bytes memory data = abi.encodeWithSignature(
+            "withdraw(uint256,address,address)",
+            10,
+            WITHDRAW_HANDLER,
+            avatar
+        );
+        vm.prank(manager);
+        role.execTransactionWithRole(sUSN, 0, data, 0, ROLE_KEY, false);
+
+        // Test should revert if recipient is not withdrawal handler
+        data = abi.encodeWithSignature(
+            "withdraw(uint256,address,address)",
+            10,
+            address(0xdead),
+            avatar
+        );
+        vm.expectRevert();
+        vm.prank(manager);
+        role.execTransactionWithRole(sUSN, 0, data, 0, ROLE_KEY, false);
+    }
+
+    function test_claim_withdrawal() public {
+        // Test claiming withdrawal from withdrawal handler
+        bytes memory data = abi.encodeWithSignature(
+            "claimWithdrawal(uint256)",
+            42
+        );
+        vm.prank(manager);
+        role.execTransactionWithRole(WITHDRAW_HANDLER, 0, data, 0, ROLE_KEY, false);
     }
 }
